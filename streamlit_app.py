66864da68c15de3659f5a5325b70be1280ed3dc4
guzmanben16 <40770142+guzmanben16@users.noreply.github.com>
2024-07-29 16:51:57 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -49,7 +49,7 @@ def main_gpt3emailgen():
                 count += 1
     if count > 0:
         title = foa_title
-        with st.expander(title, expanded=True):
+        with st.expander("SECTION - Email Output", expanded=True):
             st.markdown(foa_report)  #output the results
                
                 
