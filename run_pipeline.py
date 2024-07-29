868ce9883ca16943ec60af7ce412f8efd1e6c067
guzmanben16 <40770142+guzmanben16@users.noreply.github.com>
2024-07-29 19:02:04 -0400

    Update run_pipeline.py

--- run_pipeline.py
+++ run_pipeline.py
@@ -280,7 +280,7 @@ def run_analysis(url_list, current_datestr):
         gpt_output_dict = {foa_title:run_genai(prompt)}
         batch_output_dict.update(gpt_output_dict)
         
-    return batch_output_dict, foa_title
+    return batch_output_dict
 
 
 def combine_reports(batch_output_dict):
@@ -294,7 +294,7 @@ def combine_reports(batch_output_dict):
         final_report += '\n'
         final_report += '\n'
     
-    return final_report
+    return foa_name, final_report
 
 def run_pipeline(url_list):
     os.environ['TZ'] = 'US/Eastern'
@@ -303,11 +303,11 @@ def run_pipeline(url_list):
     code_version = '0.0.1'
     model_version = '0.0.1'
     
-    batch_output_dict, foa_title = run_analysis(url_list, current_datestr)
-    final_report = combine_reports(batch_output_dict)
+    batch_output_dict = run_analysis(url_list, current_datestr)
+    foa_name, final_report = combine_reports(batch_output_dict)
     save_final_report(batch_output_dict)
     
-    return foa_title, final_report
+    return foa_name, final_report
 
 if __name__ == "__main__":
-    foa_title, final_report = run_pipeline(url_list)
+    foa_name, final_report = run_pipeline(url_list)
