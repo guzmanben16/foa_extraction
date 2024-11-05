ade4913b6ab77384aa71bbe535dd353cc1e52ad5
guzmanben16 <40770142+guzmanben16@users.noreply.github.com>
2024-11-05 13:28:40 -0500

    Update config.py

--- utils/config.py
+++ utils/config.py
@@ -1,2 +1,5 @@
-api_key="AIzaSyBFVDUIf7Qtc-Oz8OupRadhFD7IO9Qax_I"
-model = 'gemini-1.5-flash'
+#api_key="AIzaSyBFVDUIf7Qtc-Oz8OupRadhFD7IO9Qax_I"
+#model = 'gemini-1.5-flash'
+
+api_key="AIzaSyBOcYN3d3LiyCHsdwIzbpfBkQckJ3jN0jo"
+model = 'gemini-1.5-pro'
